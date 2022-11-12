6e90de62723262cc6e94ec913944f635e1860e04
Rudolf Olah <rudolf.olah.to@gmail.com>
2022-11-12 16:17:10 -0500

    riskoracle: gathering quicknode data on nfts and token balances for training

--- riskoracle/01_gather.js
+++ riskoracle/01_gather.js
@@ -9,6 +9,26 @@ const provider = new ethers.providers.JsonRpcProvider({
     "x-qn-api-version": 1,
   },
 });
+const walletTokenBalances = {};
+const walletHighRiskNftHoldings = {};
+/*
+Based on an examination of some wallet owners of BAYC NFTs, we found some contracts for ERC721 and ERC20 that could be
+seen as high risk. We will use this list to increase the risk level of any wallet that owns one of these tokens.
+
+Sources:
+- https://etherscan.io/address/0xc883a79e8e4594c4f89434edb754a10da2311139
+- https://etherscan.io/address/0x6b92686c40747c85809a6772d0eda8e22a77c60c
+ */
+const highRiskTokens = [
+  "0x59591675E0C85258A917E80c572131a3C257A953",
+  "0x4d224452801ACEd8B2F0aebE155379bb5D594381",
+  "0xa2690792b61CA1bc5b27Daaab2b750e499EC91e7",
+];
+
+const highRiskNfts = [
+  "0xDA60730E1feAa7D8321f62fFb069eDd869E57D02",
+];
+
 // const history = new provider.getHistory(address);
 async function gatherNftTransfers(collectionContractAddress, collectionTokenId) {
   const data = await provider.send("qn_getTransfersByNFT", {
@@ -18,24 +38,29 @@ async function gatherNftTransfers(collectionContractAddress, collectionTokenId) 
     perPage: 10,
   });
 
-  const highRiskAddresses = [
-    "0x59591675E0C85258A917E80c572131a3C257A953",
-    "0x4d224452801ACEd8B2F0aebE155379bb5D594381"
-  ];
-
   for (let transfer of data["transfers"]) {
     let riskLevel = 1;
-    for (let address of highRiskAddresses) {
-      const highRiskResponse = await provider.send("qn_getWalletTokenTransactions", {
-        "address": transfer["from"] === "0x0000000000000000000000000000000000000000" ? transfer["to"] : transfer["from"],
-        "contract": address,
-        "page": 1,
-        "perPage": 10,
-      });
-      console.log(highRiskResponse);
-      if (highRiskResponse["totalItems"] > 0) {
+    const wallet = transfer["from"] === "0x0000000000000000000000000000000000000000" ? transfer["to"] : transfer["from"];
+    const walletTokenBalanceResponse = Object.hasOwn(walletTokenBalances, wallet) ? walletTokenBalances[wallet] : await provider.send("qn_getWalletTokenBalance", { wallet });
+    // console.log(walletTokenBalanceResponse);
+    walletTokenBalances[wallet] = walletTokenBalanceResponse;
+    for (let asset of walletTokenBalanceResponse["assets"]) {
+      if (highRiskTokens.includes(asset["address"])) {
         riskLevel += 1;
       }
+    }
+
+    if (walletHighRiskNftHoldings[wallet] === undefined) {
+      const highRiskNftHoldingsResponse = await provider.send("qn_fetchNFTs", {
+        wallet,
+        contracts: highRiskNfts,
+        omitFields: ["provenance", "traits"],
+        page: 1,
+        perPage: 1,
+      });
+      // console.log(highRiskNftHoldingsResponse);
+      riskLevel += highRiskNftHoldingsResponse["totalItems"];
+      walletHighRiskNftHoldings[wallet] = highRiskNftHoldingsResponse;
     }
 
     const row = [
@@ -52,5 +77,9 @@ async function gatherNftTransfers(collectionContractAddress, collectionTokenId) 
 
 const CSV_HEADER = "wallet_address,contract_address,target_address,contract_method,risk_level";
 console.log(CSV_HEADER);
-await gatherNftTransfers("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", "1");
-await gatherNftTransfers("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", "2");
+for (let i = 1; i <= 10; i += 1) {
+  // BAYC
+  await gatherNftTransfers("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", i.toString());
+  // Quirklings
+  await gatherNftTransfers("0xda60730e1feaa7d8321f62ffb069edd869e57d02", i.toString());
+}
