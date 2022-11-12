31c7268509126fd6b4e27837126902506b87894d
Rudolf Olah <rudolf.olah.to@gmail.com>
2022-11-11 23:26:15 -0500

    frontend: updated loan stats

--- frontend/src/pages/Loans.tsx
+++ frontend/src/pages/Loans.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import { Box, HStack, VStack, Flex, Button, Text, Icon, Heading, Spinner, SkeletonText, Skeleton, Stat, StatNumber, StatLabel} from "@chakra-ui/react";
 import {RiAlertFill} from "react-icons/ri";
 import {Loan, LoanItem} from "../components/LoanItem";
@@ -8,25 +8,42 @@ export function Loans() {
     {nftName: "Amazing 1", rate: 5.25, amount: 1.23, unpaidAmount: 0.23, paidAmount: 1.0, numDays: 7, daysRemaining: 3 },
     ]);
 
+  const styles = useMemo(() => ({
+    stat: {
+      color: "white",
+      bgColor: "gray.700",
+      borderRadius: 20,
+      height: 120,
+      px: 2,
+      pt: 4,
+      textAlign: "center" as const,
+    },
+    statLabel: {
+      fontSize: 20,
+    },
+  }), []);
+
   return (
     <VStack>
       <Box>
-        <Text>When you fully pay back a loan, we match a portion of the gas fees and donate it to one of our three charity partner streams. These charities represent three key areas of growth and collaboration; an organization such as women who code, climate change action, animal welfare organizations including Toronto Humane Society.</Text>
-        <HStack>
-          <Stat>
+        <HStack my={2}>
+          <Stat {...styles.stat}>
             <StatNumber>1</StatNumber>
-            <StatLabel>Loans Paid</StatLabel>
+            <StatLabel {...styles.statLabel}>Loans Paid</StatLabel>
           </Stat>
-          <Stat>
+          <Stat {...styles.stat} bgColor={"teal"}>
             <StatNumber>3</StatNumber>
-            <StatLabel>Loans Unpaid</StatLabel>
+            <StatLabel {...styles.statLabel}>Loans Unpaid</StatLabel>
           </Stat>
-          <Stat>
+          <Stat {...styles.stat} bgColor={"teal"}>
             <StatNumber>3.63</StatNumber>
-            <StatLabel>ETH Unpaid</StatLabel>
+            <StatLabel {...styles.statLabel}>ETH Unpaid</StatLabel>
           </Stat>
         </HStack>
-        <Text as={"b"} fontSize={"sm"}><Icon as={RiAlertFill} color={"yellow.500"} />Warning: loans that are not paid in full will result in a loss of collateral.</Text>
+        <Text as={"b"} fontSize={"sm"}>
+          <Icon as={RiAlertFill} color={"yellow.500"} />
+          Warning: loans that are not paid in full will result in a loss of collateral.
+        </Text>
       </Box>
       <Box>
         <LoanItem loan={loans[0]} />
