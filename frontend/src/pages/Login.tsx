54f676654847698fa02935a7f019ff8afc930c09
Rudolf Olah <rudolf.olah.to@gmail.com>
2022-11-15 00:00:51 -0500

    frontend: updated login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Divider, Heading, Image, VStack, Text, Icon} from "@chakra-ui/react";
+import {Box, Divider, Heading, Image, VStack, Text, Icon, Center} from "@chakra-ui/react";
 import {useAccount, useConnect} from "wagmi";
 import {InjectedConnector} from "wagmi/connectors/injected";
 import {RiShieldCheckLine, RiGooglePlayFill, RiShieldCheckFill} from "react-icons/ri";
@@ -13,15 +13,29 @@ export function Login() {
   });
   return (
     <VStack divider={<Divider borderColor={"gray.400"}/>} spacing={8} m={4}>
-      <Box>
-        [Logo]
-        <Text>
-          It’s Your Honey. HONEYBEE LOANS is a quick and powerful way to evaluate your NFT Portfolio and access secure loans by using your NFTs as collateral. NFTs purchased from a trusted source can be instantly turned into collateral for a short-term loan.
-        </Text>
-        <Text>
-          HONEYBEE LOANS is a one stop NFT Portfolio and Loan App. Access Your Honey.
-        </Text>
-      </Box>
+      <VStack spacing={2}>
+        <Center mt={1} ml={2}>
+          <Image src={"/images/logo.png"} width={16} />
+          <Text fontFamily={"'Julius Sans One', sans-serif"} fontSize={28} fontWeight={"bold"}>
+            HONEYBEE LOANS
+          </Text>
+        </Center>
+        <Box>
+          <Text>
+            It’s Your Honey. HONEYBEE LOANS is a quick and powerful way to evaluate your NFT Portfolio and access secure loans by using your NFTs as collateral.
+          </Text>
+        </Box>
+        <Box>
+          <Text>
+            NFTs purchased from a trusted source can be instantly turned into collateral for a short-term loan.
+          </Text>
+        </Box>
+        <Box>
+          <Text>
+            HONEYBEE LOANS is a one stop NFT Portfolio and Loan App. Access Your Honey.
+          </Text>
+        </Box>
+      </VStack>
       <Box>
         <Box textAlign={"center"}>
           <ConnectToWalletButton />
@@ -32,10 +46,12 @@ export function Login() {
         </Box>
       </Box>
       <Box>
-        <Heading as="h2" size={"lg"} textAlign={"center"}>Success Stories</Heading>
-        <Image src={"/images/nft-example-0.png"} boxSize={100} />
-        <p>This NFT was used as collateral for a 7 day loan. The borrower received 1 HPT with an interest rate of 4%.
-          They fully paid the loan and their NFT was returned to them.</p>
+        <Heading as="h2" size={"lg"} textAlign={"center"}>Success Story</Heading>
+        <Center>
+          <Image src={"/images/nft-example-7.png"} boxSize={"35%"} />
+          <Text p={4} fontSize={13}>This NFT was used as collateral for a 7-day loan. The borrower received 1.00 HPT with an interest rate of 4.12%.
+            They fully paid the loan and their NFT was returned to them.</Text>
+        </Center>
       </Box>
     </VStack>
   );
